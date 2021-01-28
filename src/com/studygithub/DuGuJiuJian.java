e41d2167a3a18507b1a5b0ee044857019ef5a84e
yuebuqun <573509796@qq.com>
2021-01-29 00:50:51 +0800

    更新剑法第四招

--- src/com/studygithub/DuGuJiuJian.java
+++ src/com/studygithub/DuGuJiuJian.java
@@ -3,9 +3,10 @@ package com.studygithub;
 public class DuGuJiuJian {
 
 	public static void main(String[] args) {
-		System.out.println("第一招-----荡剑式！！！！");
+		System.out.println("第一招------荡剑式！！！！");
 		System.out.println("第二招------离剑式!!!!!");
 		System.out.println("第三招------驭剑式！！！！");
+		System.out.println("第四招------破鞭式！！！！");
 
 	}
 
